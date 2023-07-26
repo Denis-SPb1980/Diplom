d85db54a1267b04d7e631e7b30852d12198d01a0
Comp <Comp@DESKTOP-K33JL6S>
2023-07-27 00:01:36 +0300

    Добавьте файлы проекта.

---
+++ Diplom/BussinesObject/PageObjects/BasePage.cs
@@ -0,0 +1,20 @@
+﻿using Diplom.Core;
+using OpenQA.Selenium;
+
+
+namespace Diplom.BussinesObject.PageObjects
+{
+    public abstract class BasePage
+    {
+        protected IWebDriver driver;
+
+        public BasePage()
+        {
+            driver = Browser.Instance.Driver;
+        }
+
+        public abstract BasePage OpenPage();
+    }
+       
+}
+
---
+++ Diplom/Core/Browser.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplom.Core
+{
+    internal class Browser
+    {
+        private static Browser instance = null;
+        private IWebDriver driver;
+        public IWebDriver Driver { get { return driver; } }
+
+        public static Browser Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Browser();
+                }
+                return instance;
+            }
+        }
+    }
